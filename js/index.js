03d9423c703f59b0e1fc8d3cfa4382a3763a5143
Hien-Minh Le <minkdono@gmail.com>
2022-05-24 16:10:03 +0700

    done unresponsive / scroll nav

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        nav: true,
+        loop: true,
+        dots: false,
+        margin: 50,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+       }
+    })          
+});
+
